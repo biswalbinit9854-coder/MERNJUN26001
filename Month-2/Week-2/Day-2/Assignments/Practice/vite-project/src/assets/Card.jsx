77eadfc0a1a8a387b16d111729d9812fe5934f5a
Binit Kumar Biswal <biswalbinit9854@gmail.com>
2026-07-08 10:36:41 +0530

    Fix optional chaining for title and description

--- Month-2/Week-2/Day-2/Assignments/Practice/vite-project/src/assets/Card.jsx
+++ Month-2/Week-2/Day-2/Assignments/Practice/vite-project/src/assets/Card.jsx
@@ -7,9 +7,9 @@ function Card({ car, bike, volvo }) {
       <img src={car?.img || bike?.img || volvo?.img} alt="" className="h-30 w-30 rounded-full" />
     </div>
 <p className="text-center font-bold text-gray-700">
-  {car?.title || bike?.title || volvo?.title}
+  {car.title || bike.title || volvo.title}
 </p>
-<p className="text-gray-500">{car?.description || bike?.description || volvo?.description}</p>
+<p className="text-gray-500">{car.description || bike.description || volvo.description}</p>
 <button className="p-3 w-full bg-black rounded-xl mt-2 text-white font-bold hover:bg-black-400 cursor-pointer">
   View Page
 </button>
