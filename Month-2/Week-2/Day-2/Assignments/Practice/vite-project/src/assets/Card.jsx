b633639dcf942adf4c6b4df30f18346a94e4bf34
Binit Kumar Biswal <biswalbinit9854@gmail.com>
2026-07-08 10:38:22 +0530

    Fix image source access in Card component

--- Month-2/Week-2/Day-2/Assignments/Practice/vite-project/src/assets/Card.jsx
+++ Month-2/Week-2/Day-2/Assignments/Practice/vite-project/src/assets/Card.jsx
@@ -4,7 +4,7 @@ function Card({ car, bike, volvo }) {
   return (
     <div className="w-[250px] p-2 shadow-2xl rounded-lg hover:scale-105 transition-all duration-500 place-items-center m-2">
      <div className=" w-full flex justify-center">
-      <img src={car?.img || bike?.img || volvo?.img} alt="" className="h-30 w-30 rounded-full" />
+      <img src={car.img || bike.img || volvo.img} alt="" className="h-30 w-30 rounded-full" />
     </div>
 <p className="text-center font-bold text-gray-700">
   {car.title || bike.title || volvo.title}
